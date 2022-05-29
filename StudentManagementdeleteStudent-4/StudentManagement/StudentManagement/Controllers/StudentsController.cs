31a9c77c09fca1beb4319176fa40ff20008bcdca
EkremSalaz <salaz0040@gmail.com>
2022-05-29 13:20:36 +0300

    ilk commit

---
+++ StudentManagementdeleteStudent-4/StudentManagement/StudentManagement/Controllers/StudentsController.cs
@@ -0,0 +1,77 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using StudentManagement.DomainModels;
+using StudentManagement.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentManagement.Controllers
+{
+    [ApiController]
+    public class StudentsController : Controller
+    {
+        private readonly IStudentRepository studentRepository;
+        private readonly IMapper mapper;
+        public StudentsController(IStudentRepository studentRepository, IMapper mapper)
+        {
+            this.studentRepository = studentRepository;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        [Route("[controller]")]
+        public async Task<IActionResult> GetAllStudentsAsync()
+        {
+            var students= await studentRepository.GetStudentsAsync();
+
+            return Ok(mapper.Map<List<Student>>(students));
+        }
+
+        [HttpGet]
+        [Route("[controller]/{studentId:guid}")]
+        public async Task<IActionResult> GetAllStudentAsync([FromRoute] Guid studentId)
+        {
+            var student = await studentRepository.GetStudentAsync(studentId);
+
+            if(student == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(mapper.Map<Student>(student));
+        }
+
+        [HttpPut]
+        [Route("[controller]/{studentId:guid}")]
+        public async Task<IActionResult> UpdateStudentAsync([FromRoute] Guid studentId,[FromBody] UpdateStudentRequest request)
+        {
+            if(await studentRepository.Exists(studentId))
+            {
+                var updatedStudent = await studentRepository.UpdateStudent(studentId, mapper.Map<DataModels.Student>(request));
+                if (updatedStudent !=  null)
+                {
+                    return Ok(mapper.Map<Student>(updatedStudent));
+                }
+            }
+            return NotFound();          
+        }
+
+
+        [HttpDelete]
+        [Route("[controller]/{studentId:guid}")]
+        public async Task<IActionResult> DeleteStudentAsync([FromRoute] Guid studentId)
+        {
+            if (await studentRepository.Exists(studentId))
+            {
+                var student = await studentRepository.DeleteStudent(studentId);
+                if (student != null)
+                {
+                    return Ok(mapper.Map<Student>(student));
+                }
+            }
+            return NotFound();
+        }
+    }
+}
---
+++ student-admin-ui/src/app/students/student.service.ts
@@ -0,0 +1,45 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Student } from '../models/api-models/student.model';
+import { UpdateStudentRequest } from '../models/api-models/updateStudentRequest.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StudentService {
+
+  private baseApiUrl = 'https://localhost:44381';
+
+  constructor(private httpClient:HttpClient) { }
+
+  getStudents(): Observable<Student[]>{
+    return this.httpClient.get<Student[]>(this.baseApiUrl+'/Students');
+  }
+
+
+  getStudent(studentId:string | null): Observable<Student>{
+    return this.httpClient.get<Student>(this.baseApiUrl+'/students/'+studentId);
+  }
+
+  updateStudent(studentId:string,studentRequest:Student): Observable<Student>{
+    const updateStudentRequest : UpdateStudentRequest ={
+      firstName: studentRequest.firstName,
+      lastName: studentRequest.lastName,
+      dateOfBirth: studentRequest.dateOfBirth,
+      email: studentRequest.email,
+      mobile: studentRequest.mobile,
+      genderId: studentRequest.genderId,
+      physicalAddress: studentRequest.address.physicalAddress,
+      postalAddress: studentRequest.address.postalAddress
+    }
+    return this.httpClient.put<Student>(this.baseApiUrl+'/students/'+studentId,updateStudentRequest);
+  }
+
+
+  deleteStudent(studentId:string): Observable<Student>{
+    return this.httpClient.delete<Student>(this.baseApiUrl+'/students/'+studentId);
+  }
+
+
+}
